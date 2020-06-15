efa7d5447086235c62cfd580fd8012d70f23804c
MadanKumar <madan739@gmail.com>
2020-06-15 23:03:46 +0530

    Add recipe UI implementation

--- src/main/java/mk/springframework/controllers/RecipeController.java
+++ src/main/java/mk/springframework/controllers/RecipeController.java
@@ -1,14 +1,17 @@
 package mk.springframework.controllers;
 
-import mk.springframework.domain.Recipe;
+import mk.springframework.commands.RecipeCommand;
 import mk.springframework.services.RecipeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class RecipeController {
+
     private final RecipeService recipeService;
 
     public RecipeController(RecipeService recipeService) {
@@ -17,8 +20,22 @@ public class RecipeController {
 
     @RequestMapping("/recipe/show/{id}")
     public String showById(@PathVariable String id, Model model){
-        Recipe recipe = recipeService.findById(Long.valueOf(id));
-        model.addAttribute("recipe", recipe);
+
+        model.addAttribute("recipe", recipeService.findById(Long.valueOf(id)));
         return "recipe/show";
+    }
+
+    @RequestMapping("recipe/new")
+    public String newRecipe(Model model){
+        model.addAttribute("recipe", new RecipeCommand());
+
+        return "recipe/recipeform";
+    }
+
+    @PostMapping("recipe")
+    public String saveOrUpdate(@ModelAttribute RecipeCommand command){
+        RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
+
+        return "redirect:/recipe/show/" + savedCommand.getId();
     }
 }
