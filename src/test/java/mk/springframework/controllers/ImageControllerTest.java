fd5147059da9c779b64682a844d5a36573c84c29
MadanKumar <madan739@gmail.com>
2020-06-21 10:29:17 +0530

    Added Global exception handling ControllerAdvice

--- src/test/java/mk/springframework/controllers/ImageControllerTest.java
+++ src/test/java/mk/springframework/controllers/ImageControllerTest.java
@@ -36,7 +36,9 @@ class ImageControllerTest {
         MockitoAnnotations.initMocks(this);
 
         controller = new ImageController(imageService, recipeService);
-        mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+        mockMvc = MockMvcBuilders.standaloneSetup(controller)
+                .setControllerAdvice(new ControllerExceptionHandler())
+                .build();
     }
 
     @Test
@@ -94,5 +96,13 @@ class ImageControllerTest {
         byte[] responseBytes = response.getContentAsByteArray();
 
         assertEquals(s.getBytes().length, responseBytes.length);
+    }
+
+    @Test
+    public void testGetImageNumberFormatException() throws Exception {
+
+        mockMvc.perform(get("/recipe/asdf/recipeimage"))
+                .andExpect(status().isBadRequest())
+                .andExpect(view().name("400error"));
     }
 }
